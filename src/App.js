e8bd1e89de65331d9d0f7d01f5df62c0503877b8
Diego Correa <diegoco66@gmail.com>
2022-11-21 13:10:20 -0500

    feat: Todo list

--- src/App.js
+++ src/App.js
@@ -2,12 +2,27 @@ import React from "react";
 import {TodoCounter} from "./TodoCounter"
 import {TodoSearch} from "./TodoSearch"
 import {TodoCreatedButton} from "./TodoCreatedButton"
+import {TodoList} from "./TodoList"
+import {TodoItem} from "./TodoItem"
+
+const todos = [
+  {id: 1, text: "Comprar la lista de compras", completed: false},
+  {id: 2, text: "Jugar Cities Skylines", completed: false},
+  {id: 3, text: "Picar la fruta", completed: false},
+]
 
 function App() {
   return (
     <React.Fragment>
       <TodoCounter />
       <TodoSearch />
+      <TodoList>
+        {
+          todos.map(todo => (
+            <TodoItem key={todo.id} text={todo.text} />
+          ))
+        }
+      </TodoList>
       <TodoCreatedButton />
     </React.Fragment>
   );
