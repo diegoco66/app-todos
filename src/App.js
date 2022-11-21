4c7491a281c5ace651f81676e50807878fc5151c
Diego Correa <diegoco66@gmail.com>
2022-11-21 12:51:11 -0500

    feat: Create Button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import {TodoCounter} from "./TodoCounter"
 import {TodoSearch} from "./TodoSearch"
+import {TodoCreatedButton} from "./TodoCreatedButton"
 
 function App() {
   return (
     <React.Fragment>
       <TodoCounter />
       <TodoSearch />
+      <TodoCreatedButton />
     </React.Fragment>
   );
 }
